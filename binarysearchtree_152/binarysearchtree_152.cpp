fe591ba67cc0dd849422339a8cda5ec7288f81e2
alfandialamshach <fandixpc1@gmail.com>
2023-06-29 21:22:08 +0700

    Membuat void inorder

--- binarysearchtree_152/binarysearchtree_152.cpp
+++ binarysearchtree_152/binarysearchtree_152.cpp
@@ -58,6 +58,21 @@ public:
 
 	void search(string element, Node*& parent, Node*& currentNode)
 	{
+		//This function searches the current node of the specified node as well as the current node of its parent
+		currentNode = ROOT;
+		parent = NULL;
+		while ((currentNode != NULL) && (currentNode->info != element))
+		{
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
+		}
+	}
+
+	void inorder(Node* ptr)
+	{
 
 	}
 };
